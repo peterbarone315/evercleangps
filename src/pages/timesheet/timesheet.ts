40d92c8ba7b609cc41696db982be5a986130a855
Peter Barone <peterbarone@Peters-MacBook-Pro.local>
2017-11-08 15:24:57 -0500

    adjusted tabs

--- src/pages/timesheet/timesheet.ts
+++ src/pages/timesheet/timesheet.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-timesheet',
+  templateUrl: 'timesheet.html'
 })
-export class AboutPage {
+export class TimesheetPage {
 
   constructor(public navCtrl: NavController) {
 
